fc5f45fd7b4996e67c56927e6c8c15bc4d068de2
Duc Nguyen <ducnguyen201231@gmail.com>
2021-05-03 16:25:06 +0700

    sửa phần update nhân viên

--- src/Form/Xuli/KetNoiDB.java
+++ src/Form/Xuli/KetNoiDB.java
@@ -16,7 +16,7 @@ import java.util.logging.Logger;
 public class KetNoiDB {
     public static Connection getConnection(){
         Connection con=null;
-        String url="jdbc:sqlserver://localhost:1433;databaseName=QLNV_SUAXE2";
+        String url="jdbc:sqlserver://localhost:1433;databaseName=QLNV_SUAXE";
         String user="sa";
         String passwd="123";
         try {
@@ -40,11 +40,21 @@ public class KetNoiDB {
             while (rs.next()) {                
                 m=rs.getString(1);
             }
+            if (m==null) {
+                if (cv==0) {
+                    m="ad0";
+                }else if (cv==1) {
+                    m="lt0";
+                }else if (cv==2) {
+                    m="nv0";
+                }
+            }else{
             t1=m.substring(0, 2);
             t2=m.substring(2);
             int i=Integer.parseInt(t2);
             i++;
             m=t1+Integer.toString(i);
+            }
         } catch (SQLException ex) {
             Logger.getLogger(ThemNV.class.getName()).log(Level.SEVERE, null, ex);
         }
